0d03efc0838ef5c0d8b77360a8bbc7481c00e0d6
MarcosArmando <narvaezaldana@gmail.com>
2026-02-06 19:15:37 -0600

    PORTFOLIO-001 add name to first app version

--- my-portfolio/app/page.tsx
+++ my-portfolio/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-          <p>Test first attempt website</p>
+          <p>Marcos Armando Test first attempt website</p>
     </main>
   );
 }
