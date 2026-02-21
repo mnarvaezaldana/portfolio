3a4f9755ba6b03f3d2d0f26a1ca6645af4512f8a
MarcosArmando <narvaezaldana@gmail.com>
2026-02-20 21:35:26 -0600

    FIX issues and add text

--- my-portfolio/app/page.tsx
+++ my-portfolio/app/page.tsx
@@ -30,9 +30,9 @@ export default function Home() {
     <main>
 
       <section id="home" className="section">
-        <h2>Welcome</h2>
-        <p>
-          This website presents my professional career interests and academic work.
+        <h2>Hello, My name is Marcos Armando Narvaez Aldana.</h2>
+        <p>I am a student at the University of the South, in Merida Yucatan.
+          This website presents my professional career interests and acad†emic work.
           It also includes a dynamic activity about computer parts and technology vocabulary.
         </p>
       </section>
@@ -40,12 +40,13 @@ export default function Home() {
       <section id="essay" className="section">
         <h2>How to Improve Your Skills Solving Problems</h2>
         <p>
-          Replace this text with your own academic writing created in class.
+          This will contain part of the activity
         </p>
       </section>
 
       <section id="activity" className="section">
         <h2>Technology Vocabulary Activity</h2>
+        <p>This will be the vocabulary (Glosario in Spanish) will contain more words</p>
         <p>Click the button to display a technology word:</p>
         <button onClick={showWord}>Show Word</button>
         {word && <div className="word-box">{word}</div>}
