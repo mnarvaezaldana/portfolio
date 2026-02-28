67ff77f33e869d46ecb090baae0384507f91d1b6
MarcosArmando <narvaezaldana@gmail.com>
2026-02-27 23:47:10 -0600

    add document and text

--- my-portfolio/app/page.tsx
+++ my-portfolio/app/page.tsx
@@ -40,7 +40,26 @@ export default function Home() {
       <section id="essay" className="section">
         <h2>How to Improve Your Skills Solving Problems</h2>
         <p>
-          This will contain part of the activity
+          Since I've been working as a software developer, I've encountered multiple problems related to compatibility between tools versions, nevertheless
+          I've been capable of fix them just within a couple of hours after browsing on the web. However one day I faced an issue that I still remember these days
+        </p>
+        <p>
+          I was working on an android project written in Java, some changes were asked me to do by my boss, so I thought it would be something fast, since the changes
+          seemed to be small.
+        </p>
+        <p>
+          The first time I saw the logs on red telling me there was an issue I didn't think about something critical, I thought on something like a semicolon been missed,
+          so I deleted cache, clean the project and re run it, but the issue persisted. I searched on the Internet about the issue and I found nothing but recommendations,
+          I didn't find any clear solution.
+        </p>
+        <p>
+          After two days I was still unable to fix the issue, so I contacted my boss asking for help. He told me he wasn't aware tat the project had that issue, and he already
+          encountered before, so he told me how to fix it.
+        </p>
+        <p>
+          The specific steps were to download a library, clean cache, install the library and clean cache again. After those steps I was able to run the project so happily it was
+          fixed with the help from my boss. So I learnt is ok to seek for help from your superiors, that is also part of their duties and also the importance of keeping the libraries
+          updated.
         </p>
       </section>
 
